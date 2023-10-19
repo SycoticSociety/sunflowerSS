ee595c1e26db04e600647d674d1aac8070f0c023
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-10-19 13:03:25 -0400

    Update Save.tsx

--- src/features/island/hud/components/Save.tsx
+++ src/features/island/hud/components/Save.tsx
@@ -1,12 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
 import { PIXEL_SCALE } from "features/game/lib/constants";
-
 import saveIcon from "assets/icons/save.webp";
 import loadingIcon from "assets/icons/timer.gif";
 import { Context } from "features/game/GameProvider";
 import { useActor } from "@xstate/react";
 import classNames from "classnames";
 import { SUNNYSIDE } from "assets/sunnyside";
+import { saveGameState, loadGameState } from "../localStorageUtils"; // Import the utility functions
 
 type ButtonState = "unsaved" | "inProgress" | "saved";
 
@@ -22,8 +22,7 @@ export const Save: React.FC = () => {
   const savedWithoutError = playing && !hasUnsavedProgress;
 
   const [enableButton, setEnableButton] = useState<boolean>(false);
-  const [disableSaveButtonTimer, setDisableSaveButtonTimer] =
-    useState<number>();
+  const [disableSaveButtonTimer, setDisableSaveButtonTimer] = useState<number>();
 
   const showSaved = savedWithoutError && enableButton;
   const buttonState: ButtonState = autoSaving
@@ -51,8 +50,40 @@ export const Save: React.FC = () => {
     return () => clearTimeout(disableSaveButtonTimer);
   }, [playing && !hasUnsavedProgress]);
 
+  // Function to save the game state
+  const saveGameState = (gameStateToSave) => {
+    const serializedState = JSON.stringify(gameStateToSave);
+    localStorage.setItem('gameState', serializedState);
+  };
+
+  // Function to load the saved game state
+  const loadGameState = () => {
+    const serializedState = localStorage.getItem('gameState');
+    if (serializedState) {
+      return JSON.parse(serializedState);
+    }
+    return null;
+  };
+
+  // Handle the save button click
   const save = () => {
     gameService.send("SAVE");
+
+    // Get the current game state from your context or component state
+    const currentGameState = gameState.context; // Modify this to get your game state
+
+    // Save the game state
+    saveGameState(currentGameState);
+  };
+
+  // Handle the load button click
+  const loadSavedGame = () => {
+    const loadedGameState = loadGameState();
+
+    if (loadedGameState) {
+      // Update the game state with the loaded data (replace this with your logic)
+      gameService.send({ type: "LOAD_SAVED_GAME", loadedGameState });
+    }
   };
 
   return (
@@ -68,58 +99,10 @@ export const Save: React.FC = () => {
         width: `${PIXEL_SCALE * 22}px`,
       }}
     >
-      <img
-        src={SUNNYSIDE.ui.round_button}
-        className="absolute"
-        style={{
-          width: `${PIXEL_SCALE * 22}px`,
-        }}
-      />
+      {/* ... (existing code) */}
 
-      {buttonState === "unsaved" && (
-        <img
-          src={saveIcon}
-          className="absolute"
-          style={{
-            top: `${PIXEL_SCALE * 4}px`,
-            left: `${PIXEL_SCALE * 5}px`,
-            width: `${PIXEL_SCALE * 12}px`,
-          }}
-        />
-      )}
-      {buttonState === "inProgress" && (
-        <img
-          src={loadingIcon}
-          className="absolute"
-          style={{
-            top: `${PIXEL_SCALE * 5}px`,
-            left: `${PIXEL_SCALE * 7}px`,
-            width: `${PIXEL_SCALE * 8}px`,
-          }}
-        />
-      )}
-      {buttonState === "saved" && (
-        <img
-          src={SUNNYSIDE.icons.confirm}
-          className="absolute"
-          style={{
-            top: `${PIXEL_SCALE * 5}px`,
-            left: `${PIXEL_SCALE * 5}px`,
-            width: `${PIXEL_SCALE * 12}px`,
-          }}
-        />
-      )}
-
-      <img
-        src={SUNNYSIDE.ui.round_button}
-        className={classNames("absolute", {
-          "opacity-0": enableButton,
-          "opacity-40": !enableButton,
-        })}
-        style={{
-          width: `${PIXEL_SCALE * 22}px`,
-        }}
-      />
+      {/* Add a load button in your game interface */}
+      <button onClick={loadSavedGame}>Load Saved Game</button>
     </div>
   );
 };
