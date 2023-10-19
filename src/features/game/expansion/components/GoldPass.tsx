7b7f1a36912cbd2a2643df4e0572b566e9b33e0f
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-10-18 23:53:05 -0400

    Update GoldPass.tsx

--- src/features/game/expansion/components/GoldPass.tsx
+++ src/features/game/expansion/components/GoldPass.tsx
@@ -1,21 +1,18 @@
 import { Button } from "components/ui/Button";
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 
 import { NPC_WEARABLES } from "lib/npcs";
 import { Context } from "features/game/GameProvider";
 import goldPass from "assets/announcements/gold_pass.png";
 import { Panel } from "components/ui/Panel";
-
 interface Props {
   onClose: () => void;
 }
 
 export const GoldPassModal: React.FC<Props> = ({ onClose }) => {
   const { gameService } = useContext(Context);
-  const [purchased, setPurchased] = useState(true); // Set the state to always true
 
   const price = 0.00;
-
   const Content = () => {
     return (
       <>
@@ -46,9 +43,16 @@ export const GoldPassModal: React.FC<Props> = ({ onClose }) => {
           <Button className="mr-1" onClick={onClose}>
             No thanks
           </Button>
-          {purchased ? ( // Always display "Purchased"
-            <span className="text-green-500">Purchased</span>
-          )}
+          <Button
+            onClick={() => {
+              gameService.send("PURCHASE_ITEM", {
+                name: "Gold Pass",
+              });
+              onClose();
+            }}
+          >
+            {`Buy now $${price}`}
+          </Button>
         </div>
         <div className="flex justify-center my-0.5">
           <span className="text-xxs italic">{`Price is paid in $MATIC equivalent of $${price} USD`}</span>
@@ -56,10 +60,8 @@ export const GoldPassModal: React.FC<Props> = ({ onClose }) => {
       </>
     );
   };
-
   return (
     <Panel bumpkinParts={NPC_WEARABLES.grubnuk}>
       <Content />
     </Panel>
   );
-};
