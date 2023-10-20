6fe1899bc77bbab24acdfcf6ebbec6e7290cb788
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-10-20 17:50:24 -0400

    Update Ocean.tsx

--- src/features/world/ui/Ocean.tsx
+++ src/features/world/ui/Ocean.tsx
@@ -1,13 +1,6 @@
-import React from "react";
-import ocean from "assets/decorations/ocean.png";
-import { CONFIG } from "lib/config";
-import { PIXEL_SCALE } from "features/game/lib/constants";
-
-const releaseVersion = CONFIG.RELEASE_VERSION as string;
-
 export const Ocean: React.FC = ({ children }) => {
   const oceanStyle = {
-    background: `url(${ocean}) center center / cover no-repeat`,
+    background: "black", // Set the background to black
     imageRendering: "pixelated" as React.CSSProperties["imageRendering"],
     width: "100%",
     height: "100vh",
