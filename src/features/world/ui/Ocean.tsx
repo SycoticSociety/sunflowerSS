55907465d1d6bec306eba7e2cb20efe20baf1993
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-10-20 16:24:10 -0400

    Update Ocean.tsx

--- src/features/world/ui/Ocean.tsx
+++ src/features/world/ui/Ocean.tsx
@@ -8,15 +8,15 @@ const releaseVersion = CONFIG.RELEASE_VERSION as string;
 export const Ocean: React.FC = ({ children }) => {
   const oceanStyle = {
     background: `url(${ocean}) center center / cover no-repeat`,
-    imageRendering: "pixelated" as React.CSSProperties["imageRendering"], // Specify the correct type
+    imageRendering: "pixelated" as React.CSSProperties["imageRendering"],
     width: "100%",
     height: "100vh",
   };
 
   return (
     <div
+      style={oceanStyle} // Apply the oceanStyle to this div
       className="bg-blue-600 w-full h-full flex relative items-center justify-center"
-      style={oceanStyle}
     >
       {children}
     </div>
