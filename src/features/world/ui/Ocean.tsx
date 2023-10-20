6558e02e810dd3b388cacd9f480e06bbe7cc3475
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-10-20 14:30:53 -0400

    Update Ocean.tsx

--- src/features/world/ui/Ocean.tsx
+++ src/features/world/ui/Ocean.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-import ocean from "assets/decorations/GothicBg.png";
+import GothicBg from "assets/decorations/GothicBg.png";
 import { CONFIG } from "lib/config";
 import { PIXEL_SCALE } from "features/game/lib/constants";
 
 const releaseVersion = CONFIG.RELEASE_VERSION as string;
 
-export const Ocean: React.FC = ({ children }) => {
+export const GothicBg: React.FC = ({ children }) => {
   return (
     <div
-      className="bg-blue-600 w-full bg-repeat h-full flex relative items-center justify-center"
+      className="bg-Black-600 w-full bg-repeat h-full flex relative items-center justify-center"
       style={{
-        backgroundImage: `url(${ocean})`,
+        backgroundImage: `url(${GothicBg})`,
         backgroundSize: `${64 * PIXEL_SCALE}px`,
         imageRendering: "pixelated",
       }}
