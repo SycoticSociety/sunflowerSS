114ea861710aff067041604ece47d330ab2f8cef
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-10-20 14:14:20 -0400

    Update Ocean.tsx

--- src/features/world/ui/Ocean.tsx
+++ src/features/world/ui/Ocean.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import ocean from "assets/decorations/ocean.webp";
+import ocean from "assets/decorations/GothicBg.png";
 import { CONFIG } from "lib/config";
 import { PIXEL_SCALE } from "features/game/lib/constants";
 
